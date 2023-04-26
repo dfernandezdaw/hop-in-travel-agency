0fca6c5cb1d374a5a5c462e58e8cc84f09f339fc
David <dfernandez.alumno@iesrafaelalberti.es>
2023-04-26 13:21:58 +0200

    add user model

---
+++ backend/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [6, 'Password must be at least 6 characters long'],
+    },
+    profilePicture: {
+      type: String,
+      default: '',
+    },
+    role: {
+      type: String,
+      default: 'user',
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('User', userSchema)
