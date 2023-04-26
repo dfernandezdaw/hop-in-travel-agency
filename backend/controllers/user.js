92b9e632fe925a281abbd18c717549a5c748ee05
David <dfernandez.alumno@iesrafaelalberti.es>
2023-04-26 14:51:59 +0200

    add user controller

---
+++ backend/controllers/user.js
@@ -0,0 +1,62 @@
+const User = require('../models/User')
+
+// Get all users
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (error) {
+    res.status(400).json({ message: error })
+  }
+}
+
+// Get single user
+const getUser = async (req, res) => {
+  const { id } = req.params
+  try {
+    const user = await User.findById(id)
+    res.json({ message: 'User created successfully', data: user })
+  } catch (error) {
+    res.status(400).json({ message: error })
+  }
+}
+
+// Create user
+const createUser = async (req, res) => {
+  const { username, email, password } = req.body
+  try {
+    const user = await User.create({ username, email, password })
+    res.json(user)
+  } catch (error) {
+    res.status(400).json({ message: error })
+  }
+}
+
+// Update user
+const updateUser = async (req, res) => {
+  const { id } = req.params
+  const { username, email, password } = req.body
+  try {
+    const user = await User.findByIdAndUpdate(
+      id,
+      { username, email, password },
+      { new: true }
+    )
+    res.json({ message: 'User updated successfully', data: user })
+  } catch (error) {
+    res.status(400).json({ message: error })
+  }
+}
+
+// Delete user
+const deleteUser = async (req, res) => {
+  const { id } = req.params
+  try {
+    const user = await User.findByIdAndDelete(id)
+    res.json({ message: 'User deleted successfully' })
+  } catch (error) {
+    res.status(400).json({ message: error })
+  }
+}
+
+module.exports = { getUsers, getUser, createUser, updateUser, deleteUser }
