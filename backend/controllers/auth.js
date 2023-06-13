d7f7602a07eadb3f7874d6e937acb6384215ce1b
David <dfernandez.alumno@iesrafaelalberti.es>
2023-06-13 04:20:36 +0200

    modified response on login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -48,6 +48,7 @@ const login = async (req, res) => {
           username: user.username,
           email: user.email,
           isEmailVerified: user.verified,
+          role: user.role,
         })
       } else {
         res.status(401).json({ message: 'Invalid password' })
