5f7294d71dcd35a1c22df5cd3ca815c65f74fe0c
David <dfernandez.alumno@iesrafaelalberti.es>
2023-06-16 07:40:08 +0200

    404 page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/not-found.css'
 
 const NotFound = () => {
-  return <div>404</div>
+  return (
+    <section>
+      <div className='not-found-page'>
+        <h1 className='not-found-page__title'>404 Page Not Found</h1>
+        <p className='not-found-page__message'>
+          The requested page does not exist.
+        </p>
+        <Link to='/' className='not-found-page__button'>
+          Go to Homepage
+        </Link>
+      </div>
+    </section>
+  )
 }
 
 export default NotFound
